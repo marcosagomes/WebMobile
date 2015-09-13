e77d83eb123f66c71cc7a62e4198ff7dc7724d95
marcosagomes <alves.mgomes@gmail.com>
2015-09-13 17:22:40 -0300

    Categoria produdo menu

---
+++ WebMobile/web/js/funcoes-jquery.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+// Permite a execucao inicial das funcionalidades javascript
+$(function () { // Abreviação para $(document).ready(function() { ... });
+
+    $('.special.cards .image').dimmer({
+        on: 'hover'
+    });
+});
