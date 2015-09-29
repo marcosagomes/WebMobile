f45e3f4e4c8f5c6a1c716a7ea3d681aed277a8cc
marcosagomes <alves.mgomes@gmail.com>
2015-09-29 02:16:36 -0300

    Menu cel OKA

--- DraftWeb/DraftWeb-web/target/DraftWeb-web-1.0-SNAPSHOT/src/js/funcoes-jquery.js
+++ DraftWeb/DraftWeb-web/target/DraftWeb-web-1.0-SNAPSHOT/src/js/funcoes-jquery.js
@@ -9,41 +9,47 @@
 $(function () { // Abreviação para $(document).ready(function() { ... });
 
 //    Dimmer nas imagens
-  $('.special.cards .image').dimmer({
-    on: 'hover'
-  });
+    $('.special.cards .image').dimmer({
+        on: 'hover'
+    });
 //    Apresentar aquele alert (modal) daora
-  $('#showModal').click(function () {
-    $('.basic.modal')
-            .modal('show')
-            ;
-  });
+    $('#showModal').click(function () {
+        $('.basic.modal')
+                .modal('show')
+                ;
+    });
 //    Quando clicar no carrinho fazer uma animacao
-  $('.addCarrinho').click(function () {
-    $(this).blur();
-    var qtd = parseInt($('#labelCarrinho').text());
-    animacaoAdicionouItens(qtd);
-  });
-  // Função do Slider da pagina de detalhes
-  $('#slider').nivoSlider({});
+    $('.addCarrinho').click(function () {
+        $(this).blur();
+        var qtd = parseInt($('#labelCarrinho').text());
+        animacaoAdicionouItens(qtd);
+    });
+    // Função do Slider da pagina de detalhes
+    $('#slider').nivoSlider({});
 
-  //Função para os tabs da paginda de detalhes
-  $('.tabular.menu .item').tab();
+    //Função para os tabs da paginda de detalhes
+    $('.tabular.menu .item').tab();
 
-  $(".addCarrinho").focus(function () {
-    $(this).removeClass("ui-state-focus");
-  });
+    $(".addCarrinho").focus(function () {
+        $(this).removeClass("ui-state-focus");
+    });
+    $(".ui.icon.button").click(function () {
+        $('.ui.labeled.icon.sidebar')
+                .sidebar('toggle')
+                ;
+    });
+
 });
 
 function animacaoAdicionouItens(quantidade) {
 //    Testando uma animacao na contagem xD
 //        var texto = parseInt($('#labelCarrinho').text());
-  if (quantidade == 0) {
-    $('#labelCarrinho')
-            .transition('browse right');
-  }
+    if (quantidade == 0) {
+        $('#labelCarrinho')
+                .transition('browse right');
+    }
 
-  $('#labelCarrinho')
-          .text(quantidade + 1)
-          .transition('jiggle');
+    $('#labelCarrinho')
+            .text(quantidade + 1)
+            .transition('jiggle');
 }
